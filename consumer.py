6a3fe9fc725767043f427228ba4a83663cda64a7
Andrii Oriekhov <andriyorehov@gmail.com>
2025-07-05 13:11:24 +0300

    write to sqlite3

--- consumer.py
+++ consumer.py
@@ -1,6 +1,24 @@
 import json
+import sqlite3
 
 from kafka import KafkaConsumer
+
+
+conn = sqlite3.connect("sensor_data.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS sensor_readings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        temperature REAL,
+        humidity REAL,
+        timestamp REAL
+    )
+"""
+)
+conn.commit()
+
 
 consumer = KafkaConsumer(
     "sensor-data",
@@ -13,3 +31,15 @@ consumer = KafkaConsumer(
 
 for msg in consumer:
     print("Received:", msg.value)
+
+    try:
+        cursor.execute(
+            """
+              INSERT INTO sensor_readings (temperature, humidity, timestamp)
+              VALUES (?, ?, ?)
+              """,
+            (msg.value["temperature"], msg.value["humidity"], msg.value["timestamp"]),
+        )
+        conn.commit()
+    except Exception as e:
+        print("DB error:", e)
